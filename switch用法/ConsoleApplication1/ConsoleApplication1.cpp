66896a3044db45c0b69b45f99f4ad1f6e3ba191d
1265823 <1450599428@qq.com>
2021-06-08 22:39:07 +0800

    ..

---
+++ switch用法/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int val;
+
+	printf("请选择关卡:");
+	scanf("%d", &val);
+
+	switch (val)
+	{
+	case 1:
+		printf("第一关\n");
+		break;
+	case 2:
+		printf("第二关\n");
+		break;
+	case 3:
+		printf("第三关\n");
+		break;
+	default:
+		printf("暂无新关卡\n");
+		break;
+	}
+}
