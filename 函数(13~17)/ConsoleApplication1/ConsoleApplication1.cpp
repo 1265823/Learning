75254e55ac0940a20ae84a35b25820c00875d3dd
1265823 <1450599428@qq.com>
2021-06-24 18:27:33 +0800

    .

---
+++ 函数(13~17)/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+/*
+void f(int i);
+void g(void)
+{
+	f(5);
+}
+void f(int i)
+{
+//	int i = 99;  形参i的重定义
+
+	printf("i = %d\n", i);
+	return ;
+	
+}
+
+int main(void)
+{
+	int i = 10;   //与9行代码重定义
+	g();
+	return 0;
+}
+*/
+
+/*
+// 为何是错的，因为k的声明在32行，而函数g在27行到30行中，k并没有声明
+void g()
+{
+	printf("k = %d\n", k);
+}
+
+int k = 1000; 
+
+void f(void)
+{
+	g();
+	printf("k = %d\n", k);
+}
+
+int main(void)
+{
+	f();
+	return 0;
+}
+*/
