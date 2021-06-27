6f70af31849fd559b48e2dfb550315d4ae2e10b2
1265823 <1450599428@qq.com>
2021-06-27 15:36:19 +0800

    .

---
+++ 指针9_复习指针知识/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+/*
+int main(void)
+{
+	int* p; //等价与int *p；也等价于int * p
+	int i = 5;
+	char ch = 'A';
+
+	p = &i; //*p以p的内容为地址的变量
+	*p = 99;
+	printf("i = %d, *p = %d\n", i, *p);
+
+	//p = &ch;  error
+	//p = ch;  error
+	//p = 5;  error
+}
+*/
+
+
+
+
+
+
+
+
+
+
+void swap_1(int* p, int* q) //形参名字是p和q，接收实参数据的是p和q，而不是*p和*q
+{
+	int t;
+	t = *p;
+	*p= *q;
+	*q= t;
+}
+
+int main(void)
+{
+	int a = 3;
+	int b = 5;
+
+	swap_1(&a, &b);
+	printf("a = %d,b = %d\n", a, b);
+}
