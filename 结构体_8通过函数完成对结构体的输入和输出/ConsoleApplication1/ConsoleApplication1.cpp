71f503441c152ed77ff2f6e30a17fea7295c531f
1265823 <1450599428@qq.com>
2021-07-14 19:53:38 +0800

    .

---
+++ 结构体_8通过函数完成对结构体的输入和输出/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+struct Student
+
+{
+	int age;
+	char sex;
+	char name[100];
+ };
+void InputStudent(struct Student *);
+void OutputStudent(struct Student *);
+int main(void)
+{
+	struct Student st;
+	printf("%d\n", sizeof(st));
+	
+	InputStudent(&st); //对结构体变量输入   必须发送st的地址
+	OutputStudent(&st);//对结构体变量输出  可以发送st的地址，也可以发送内容 为减少内存和提高速度，推荐发送地址
+
+	return 0;
+}
+
+void OutputStudent(struct Student *pst)
+{
+	printf("%d %c %s\n", (*pst).age, pst->sex, pst->name);
+}
+
+void InputStudent(struct Student *pstu)
+{
+	(*pstu).age = 10;
+	strcpy(pstu->name, "张三");
+	pstu->sex = 'F';
+}
